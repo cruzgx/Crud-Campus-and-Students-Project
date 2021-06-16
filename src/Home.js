1d0b09d63c20d5a8b697603c8a412b209db81d54
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-06-15 21:36:04 -0400

    Added styling to home page
    Correction of inline elements will follow, also needs a color switch in between Campuses and Students section of homepage

--- src/Home.js
+++ src/Home.js
@@ -13,48 +13,49 @@ class Home extends React.Component{
     //methods go here
     render(){
         return(
-            <div className='header'>
-                <div className='front-text'>
+            <div className='top-level-container'>
+                <div className='header'>
+                    <div className='front-text'>
                     <h1 className='page-heading'>Public Campus Search Tool</h1>
                     <p>Our site provides all your school data needs from campuses to students</p>
                 </div>
+                </div>
                 <section className='site-info'>
                 <h2>Campuses</h2>              
-                <p>Click here to see a listing of all campuses</p>
+                <p>Click here to be redirected to a listing of all campuses</p>
                 <Link to='/Campus'>
-                    <button type='button'>Go</button>
+                    <button type='button' className='button'>Listing of All Campuses</button>
                 </Link>
   
                 <p>Click below to be redirected to a single campus view</p>
                 <Link to='/Campus'>
-                    <button>Single Campus View </button>
+                    <button type='button' className='button'>Single Campus View </button>
                 </Link>
        
 
                 <h2>Students</h2>
                 <p>Our database holds a record of many students across the <br></br>
-                    United States. Below you'll find resources to help acess the <br></br>
-                    best schools and student performance
+                    United States. Below you'll find resources to help acess  <br></br>
+                    student metrics among many other metrics 
                 </p>
 
                 <br></br>
                 <p>Take a look at our directory of all of the students in our <br></br>
                 in our database here</p>
                 <Link to='/Students'>
-                    <button>Full Student directory</button>
+                    <button type='button' className='button'>Full Student Directory</button>
                 </Link>
                 <p>Interested in looking into where a prospective student is currently
                     enrolled at? Take a look at our individual student profiles page below.
                 </p>
                 <Link to= '/Students'> 
-                <button>Individual Student Profiles</button>
+                <button type='button' className='button'>Individual Student Profiles</button>
                 </Link>
                 </section>
-                
-                <p>About us</p>
-                <p>Contanct</p>
-                <small>Company Name Example. All Rights Reserved</small>
-
+                <br></br>
+                <br></br>
+                <br></br>
+                <small className='copy-right'>My Campus. All Rights Reserved</small>
             </div>
         )
     }
